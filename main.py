cf1666e2b428d5a2c3a09d550fd8757660255704
AbduSadiq <sadiqabdurrafay@gmail.com>
2025-11-10 17:44:41 -0600

    Add emotion detection to customer service assistant

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from nrclex import NRCLex
+import uuid
 
 print("Welcome to Apple's customer service for their Texas branch. I am your virtual assistant Alex, before we begin, I will just be needing some information.")
 input("Press enter to continue...")
@@ -14,10 +15,50 @@ while True:
     print("3) Following up about an existing purchase.")
     print("4) Exit chat")
 
-    choice = input("Make a choice please: ")
+    choice = input("Make a choice please: ") 
+    emotion = NRCLex(choice)
+    top_emotion = emotion.top_emotions
+    if top_emotion:
+        print(f"(Alex detects your emotion as: {top_emotion[0][0]})")  
+    choice = choice.strip()
 
     if choice == '1':
         input("Looks like you want to start a return, may I have your order number.")
+
+        city = input("Great, Now please enter your city: ")
+        city_emotion = NRCLex(city)
+        top_city_emotion = city_emotion.top_emotions
+        if top_city_emotion:
+            print(f"(Alex detects your emotion as: {top_city_emotion[0][0]})")
+
+        stores_in_cities = {
+            "Austin": ["Domain (Northside)", "Barton Creek"],
+            "Dallas": ["NorthPark Center", "Knox Street", "Galleria Dallas" ],
+            "Houston": ["Memorial City", "Houston Galleria", "Willowbrook Mall", "Highland Village"],
+            "El Paso": ["Cielo Vista Mall"],
+            "Fort Worth": ["University Park Village"],
+            "Friendswood": ["Baybrook"],
+            "San Antonio": ["North Star", "La Cantera"],
+            "Southlake": ["Southlake Town Square"],
+            "Sugar Land": ["First Colony Mall"],
+            "The Woodlands": ["The Woodlands"]
+        }
+
+        if city in stores_in_cities:
+            print("Here are the stores available for " + city + ".")
+            for store in stores_in_cities[city]:
+                print("-" + store)
+        else:
+            print("Invalid city, please try again.")
+
+        input("Press enter to continue...")
+
+        print("Alright, I have provided you with a return ID below which you will enter at the location closest to you in your city. Return ID:", uuid.uuid4())
+        print("Have a great day, Goodbye")
+        break
+        
+    elif choice == '2':
+        print("Looks like you want to start an exchange, may I have your order number.")
 
         city = input("Great, Now please enter your city: ")
 
@@ -46,17 +87,15 @@ while True:
         print("Alright, I have provided you with a return ID below which you will enter at the location closest to you in your city. Return ID:", uuid.uuid4())
         print("Have a great day, Goodbye")
         break
-    elif choice == '2':
-        print("Looks like you want to start an exchange, may I have your order number.")
-        break
+
     elif choice == '3':
-        print("Looks like you want to follow up for an existing order, may I have your order number.")
+        orderNumber = input("Looks like you want to follow up for an existing order, may I have your order number.")
+
+        print("Perfect, your order is currently being processed at out facility and will be sent out for delivery shortly")
+
         break
     elif choice == '4':
         print("Have a great day, Goodbye")
         break
     else:
         print("Choice not valid, please try again later.")
-
-
-
